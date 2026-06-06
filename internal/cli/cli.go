be2b11444f046cf7f40d8e8349adaae107b08047
tour-zotz <>
2026-06-06 19:40:03 +0530

    Bump version to v0.1.8

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -15,7 +15,7 @@ import (
 	"github.com/john221wick/gpuSchedularSN/internal/state"
 )
 
-const Version = "0.1.7"
+const Version = "0.1.8"
 
 var GlobalState *state.State
 var ClusterMgr *cluster.NodeManager
